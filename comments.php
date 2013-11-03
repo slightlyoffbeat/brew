166a86b6ed91f729444963275774d27e8265f1d3
Dan Brown <slightlyoffbeat@gmail.com>
2013-11-03 00:07:29 -0700

    Tons of styling updates for comments

--- comments.php
+++ comments.php
@@ -19,25 +19,12 @@ The comments page for Bones
 <?php // You can start editing here. ?>
 <div id="comments">
 <?php if ( have_comments() ) : ?>
-	<h3 class="h2"><?php comments_number( __( '<span>No</span> Responses', 'bonestheme' ), __( '<span>One</span> Response', 'bonestheme' ), _n( '<span>%</span> Response', '<span>%</span> Responses', get_comments_number(), 'bonestheme' ) );?> to &#8220;<?php the_title(); ?>&#8221;</h3>
-
-	<nav class="comment-nav">
-		<ul class="clearfix">
-				<li><?php previous_comments_link() ?></li>
-				<li><?php next_comments_link() ?></li>
-		</ul>
-	</nav>
+	<h3><?php comments_number( __( '<span>No</span> Responses', 'bonestheme' ), __( '<span>One</span> Response', 'bonestheme' ), _n( '<span>%</span> Response', '<span>%</span> Responses', get_comments_number(), 'bonestheme' ) );?> to &#8220;<?php the_title(); ?>&#8221;</h3>
 
 	<ol class="commentlist">
 		<?php wp_list_comments( 'type=comment&callback=bones_comments' ); ?>
 	</ol>
 
-	<nav class="comment-nav">
-		<ul class="clearfix">
-				<li><?php previous_comments_link() ?></li>
-				<li><?php next_comments_link() ?></li>
-		</ul>
-	</nav>
 
 	<?php else : // this is displayed if there are no comments so far ?>
 
@@ -56,64 +43,75 @@ The comments page for Bones
 
 <?php if ( comments_open() ) : ?>
 
-<section id="respond" class="respond-form">
-
-	<h3 id="comment-form-title" class="h2"><?php comment_form_title( __( 'Leave a Reply', 'bonestheme' ), __( 'Leave a Reply to %s', 'bonestheme' )); ?></h3>
-
-	<div id="cancel-comment-reply">
-		<p class="small"><?php cancel_comment_reply_link(); ?></p>
-	</div>
-
-	<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
-		<div class="alert alert-help">
-			<p><?php printf( __( 'You must be %1$slogged in%2$s to post a comment.', 'bonestheme' ), '<a href="<?php echo wp_login_url( get_permalink() ); ?>">', '</a>' ); ?></p>
-		</div>
-	<?php else : ?>
-
-	<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-
-	<?php if ( is_user_logged_in() ) : ?>
-
-	<p class="comments-logged-in-as"><?php _e( 'Logged in as', 'bonestheme' ); ?> <a href="<?php echo get_option( 'siteurl' ); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url( get_permalink() ); ?>" title="<?php _e( 'Log out of this account', 'bonestheme' ); ?>"><?php _e( 'Log out', 'bonestheme' ); ?> <?php _e( '&raquo;', 'bonestheme' ); ?></a></p>
-
-	<?php else : ?>
-
-	<ul id="comment-form-elements" class="clearfix">
-
-		<li>
-			<label for="author"><?php _e( 'Name', 'bonestheme' ); ?> <?php if ($req) _e( '(required)'); ?></label>
-			<input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" placeholder="<?php _e( 'Your Name*', 'bonestheme' ); ?>" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
-		</li>
-
-		<li>
-			<label for="email"><?php _e( 'Mail', 'bonestheme' ); ?> <?php if ($req) _e( '(required)'); ?></label>
-			<input type="email" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" placeholder="<?php _e( 'Your E-Mail*', 'bonestheme' ); ?>" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
-			<small><?php _e("(will not be published)", 'bonestheme' ); ?></small>
-		</li>
-
-		<li>
-			<label for="url"><?php _e( 'Website', 'bonestheme' ); ?></label>
-			<input type="url" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" placeholder="<?php _e( 'Got a website?', 'bonestheme' ); ?>" tabindex="3" />
-		</li>
-
-	</ul>
-
-	<?php endif; ?>
-
-	<p><textarea name="comment" id="comment" placeholder="<?php _e( 'Your Comment here...', 'bonestheme' ); ?>" tabindex="4"></textarea></p>
-
-	<p>
-		<input name="submit" type="submit" id="submit" class="btn btn-primary" tabindex="5" value="<?php _e( 'Submit', 'bonestheme' ); ?>" />
-		<?php comment_id_fields(); ?>
-	</p>
-
-
-	<?php do_action( 'comment_form', $post->ID ); ?>
-
-	</form>
-
-	<?php endif; // If registration required and not logged in ?>
-</section>
 
 <?php endif; // if you delete this the sky will fall on your head ?>
+
+<nav class="comment-nav clearfix">
+  <div class="comment-prev">
+      <?php previous_comments_link(__( '<i class="fa fa-chevron-left"></i>', 'bones' ) . '  Previous Comments') ?>
+    </div>
+    <div class="comment-next">
+      <?php next_comments_link(__( 'Next Comments  ', 'standard' ) . '<i class="fa fa-chevron-right"></i>') ?>
+    </div>
+  </nav>
+
 </div> <!-- END #COMMENTS -->
+
+<div id="respond" class="respond-form">
+
+  <h3 id="comment-form-title"><?php comment_form_title( __( 'Leave a Reply', 'bonestheme' ), __( 'Leave a Reply to %s', 'bonestheme' )); ?></h3>
+
+  <div id="cancel-comment-reply">
+    <p class="small"><?php cancel_comment_reply_link(); ?></p>
+  </div>
+
+  <?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
+    <div class="alert alert-help">
+      <p><?php printf( __( 'You must be %1$slogged in%2$s to post a comment.', 'bonestheme' ), '<a href="<?php echo wp_login_url( get_permalink() ); ?>">', '</a>' ); ?></p>
+    </div>
+  <?php else : ?>
+
+  <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+
+  <?php if ( is_user_logged_in() ) : ?>
+
+  <p class="comments-logged-in-as"><?php _e( 'Logged in as', 'bonestheme' ); ?> <a href="<?php echo get_option( 'siteurl' ); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url( get_permalink() ); ?>" title="<?php _e( 'Log out of this account', 'bonestheme' ); ?>"><?php _e( 'Log out', 'bonestheme' ); ?> <?php _e( '&raquo;', 'bonestheme' ); ?></a></p>
+
+  <?php else : ?>
+
+  <ul id="comment-form-elements" class="clearfix">
+
+    <li>
+      <label for="author"><?php _e( 'Name', 'bonestheme' ); ?> <?php if ($req) _e( '(required)'); ?></label>
+      <input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" placeholder="<?php _e( 'Your Name*', 'bonestheme' ); ?>" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
+    </li>
+
+    <li>
+      <label for="email"><?php _e( 'Mail', 'bonestheme' ); ?> <?php if ($req) _e( '(required)'); ?></label>
+      <input type="email" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" placeholder="<?php _e( 'Your E-Mail*', 'bonestheme' ); ?>" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
+      <small><?php _e("(will not be published)", 'bonestheme' ); ?></small>
+    </li>
+
+    <li>
+      <label for="url"><?php _e( 'Website', 'bonestheme' ); ?></label>
+      <input type="url" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" placeholder="<?php _e( 'Got a website?', 'bonestheme' ); ?>" tabindex="3" />
+    </li>
+
+  </ul>
+
+  <?php endif; ?>
+
+  <p><textarea name="comment" id="comment" placeholder="<?php _e( 'Your Comment here...', 'bonestheme' ); ?>" tabindex="4"></textarea></p>
+
+  <p>
+    <input name="submit" type="submit" id="submit" class="btn btn-primary" tabindex="5" value="<?php _e( 'Submit', 'bonestheme' ); ?>" />
+    <?php comment_id_fields(); ?>
+  </p>
+
+
+  <?php do_action( 'comment_form', $post->ID ); ?>
+
+  </form>
+
+  <?php endif; // If registration required and not logged in ?>
+</div> <!-- END #respond -->
--- functions.php
+++ functions.php
@@ -191,7 +191,7 @@ function bones_comments( $comment, $args, $depth ) {
    $GLOBALS['comment'] = $comment; ?>
 	<li <?php comment_class(); ?>>
 		<article id="comment-<?php comment_ID(); ?>" class="clearfix comment-container">
-			<header class="comment-author vcard">
+			<div class="comment-author vcard">
 				<?php
 				/*
 					this is the new responsive optimized comment image. It used the new HTML5 data-attribute to display comment gravatars on larger screens only. What this means is that on larger posts, mobile sites don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd like to change it back, just replace it with the regular wordpress gravatar call:
@@ -205,19 +205,21 @@ function bones_comments( $comment, $args, $depth ) {
 				?>
 				<img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=64" class="load-gravatar avatar avatar-48 photo" height="64" width="64" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
 				<?php // end custom gravatar call ?>
-				<?php printf(__( '<cite class="fn">%s</cite>', 'bonestheme' ), get_comment_author_link()) ?>
-				<time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'bonestheme' )); ?> </a></time>
-				<?php edit_comment_link(__( '(Edit)', 'bonestheme' ),'  ','') ?>
-			</header>
-			<?php if ($comment->comment_approved == '0') : ?>
-				<div class="alert alert-info">
-					<p><?php _e( 'Your comment is awaiting moderation.', 'bonestheme' ) ?></p>
-				</div>
-			<?php endif; ?>
-			<section class="comment_content clearfix">
-				<?php comment_text() ?>
-			</section>
-			<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+			</div>
+      <div class="comment-content">
+        <?php printf(__( '<cite class="fn">%s</cite>', 'bonestheme' ), get_comment_author_link()) ?>
+        <time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'bonestheme' )); ?> </a></time>
+        <?php edit_comment_link(__( '(Edit)', 'bonestheme' ),'  ','') ?>
+  			<?php if ($comment->comment_approved == '0') : ?>
+  				<div class="alert alert-info">
+  					<p><?php _e( 'Your comment is awaiting moderation.', 'bonestheme' ) ?></p>
+  				</div>
+  			<?php endif; ?>
+  			<section class="comment_content clearfix">
+  				<?php comment_text() ?>
+  			</section>
+  			<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+      </div> <!-- END comment-content -->
 		</article>
 	<?php // </li> is added by WordPress automatically ?>
 <?php
